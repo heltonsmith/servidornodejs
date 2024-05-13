4b0420b2d93bb2c22c37515e5c997389c2aca152
Helton Bustos <heltonsmith@hotmail.com>
2024-05-13 12:14:43 -0400

    post

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+// Middleware para analizar el cuerpo de la solicitud JSON
+app.use(express.json());
+
 // Ruta para servir el archivo HTML
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+// Ruta para manejar la solicitud POST de datos
+app.post('/enviar-datos', (req, res) => {
+    const datosRecibidos = req.body;
+    console.log('Datos recibidos:', datosRecibidos);
+    // Puedes enviar los datos a través de WebSocket aquí si es necesario
+    res.send('Datos recibidos correctamente');
 });
 
 // Configura WebSocket para recibir datos del Arduino
